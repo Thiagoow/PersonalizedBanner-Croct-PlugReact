1754ee7f59ccd4faae15a895105bede44d8953a0
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-02 20:40:33 -0300

    Random space trimmed on class

--- src/components/BtnLink.jsx
+++ src/components/BtnLink.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BtnLink = ({ href, children }) => {
   return (
     <a
-      className={' text-firstColor font-semibold hover:text-altFirstColor'}
+      className={'text-firstColor font-semibold hover:text-altFirstColor'}
       href={href}
       target="_blank"
       rel="noopener noreferrer"
