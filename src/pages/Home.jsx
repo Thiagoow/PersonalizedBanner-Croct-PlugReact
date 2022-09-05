4bb30c47954f6db36eb6b2145d2726285134528b
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-05 17:42:44 -0300

    Added loading screen fallback

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Slot } from '@croct/plug-react'
+import LoadingScreen from './LoadingScreen'
 import Button from '../components/Button'
 
 export default function Home() {
@@ -13,28 +14,30 @@ export default function Home() {
   }
 
   return (
-    <Slot id="home-banner" fallback={fallbackBanner}>
-      {({ title, subtitle, cta }) => (
-        <main
-          className={
-            'h-screen flex flex-col items-center justify-center bg-bodyColor text-txtColor'
-          }
-        >
-          <section className={'grid gap-4 max-w-sm'}>
-            <h1 className={'text-firstColor font-bold text-3xl'}>
-              <strong className="text-altFirstColor"></strong>
-              {title}
-            </h1>
+    <React.Suspense fallback={<LoadingScreen />}>
+      <Slot id="home-banner" fallback={fallbackBanner}>
+        {({ title, subtitle, cta }) => (
+          <main
+            className={
+              'h-screen flex flex-col items-center justify-center bg-bodyColor text-txtColor'
+            }
+          >
+            <section className={'grid gap-4 max-w-sm'}>
+              <h1 className={'text-firstColor font-bold text-3xl'}>
+                <strong className="text-altFirstColor"></strong>
+                {title}
+              </h1>
 
-            <p>{subtitle}</p>
-            <Button label={cta.label} href={cta.link} />
-          </section>
+              <p>{subtitle}</p>
+              <Button label={cta.label} href={cta.link} />
+            </section>
 
-          <section className="infoImage">
-            <img src="" alt="" />
-          </section>
-        </main>
-      )}
-    </Slot>
+            <section className="infoImage">
+              <img src="" alt="" />
+            </section>
+          </main>
+        )}
+      </Slot>
+    </React.Suspense>
   )
 }
---
+++ src/pages/LoadingScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LoadingScreen() {
+  return (
+    <h1 className="grid h-screen w-screen place-items-center text-2xl font-semibold">
+      ✨ Personalizing...
+    </h1>
+  )
+}
