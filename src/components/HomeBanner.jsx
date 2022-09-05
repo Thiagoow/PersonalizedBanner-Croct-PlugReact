a38002c07495690ce0c309e7b65131c7846ad85f
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-05 15:37:03 -0300

    Temp HomeBanner data component added

---
+++ src/components/HomeBanner.jsx
@@ -0,0 +1,11 @@
+export const HomeBanner = {
+  accentTitle: 'Get more',
+  accentFirst: true,
+  title: 'out of your marketing investment!',
+  subtitle:
+    'Optimize your digital customer experience to dive more sales and happier customers.',
+  cta: {
+    label: 'Book a demo',
+    link: 'https://www.google.com'
+  }
+}
