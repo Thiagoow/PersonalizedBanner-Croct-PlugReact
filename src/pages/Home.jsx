90b5d40dcadfa7ea848fc448e24e970ebb4271a8
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-05 17:41:33 -0300

    Showing content according to user's persona :D

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,40 @@
 import React from 'react'
-import { HomeBanner } from '../components/HomeBanner'
-const { accentTitle, accentFirst, title, subtitle, cta } = HomeBanner
+import { Slot } from '@croct/plug-react'
 import Button from '../components/Button'
 
 export default function Home() {
+  const fallbackBanner = {
+    title: 'Default title',
+    subtitle: 'Default subtitle',
+    cta: {
+      label: 'Try now',
+      link: 'https://croct.com'
+    }
+  }
+
   return (
-    <main
-      className={
-        'h-screen flex flex-col items-center justify-center bg-bodyColor text-txtColor'
-      }
-    >
-      <section className={'grid gap-4 max-w-sm'}>
-        <h1 className={'text-firstColor font-bold text-3xl'}>
-          {accentFirst && (
-            <strong className="text-altFirstColor">{accentTitle}</strong>
-          )}
+    <Slot id="home-banner" fallback={fallbackBanner}>
+      {({ title, subtitle, cta }) => (
+        <main
+          className={
+            'h-screen flex flex-col items-center justify-center bg-bodyColor text-txtColor'
+          }
+        >
+          <section className={'grid gap-4 max-w-sm'}>
+            <h1 className={'text-firstColor font-bold text-3xl'}>
+              <strong className="text-altFirstColor"></strong>
+              {title}
+            </h1>
 
-          {` ${title} `}
+            <p>{subtitle}</p>
+            <Button label={cta.label} href={cta.link} />
+          </section>
 
-          {!accentFirst && (
-            <strong className="text-altFirstColor">{accentTitle}</strong>
-          )}
-        </h1>
-
-        <p>{subtitle}</p>
-        <Button label={cta.label} href={cta.link} />
-      </section>
-
-      <section className="infoImage">
-        <img src="" alt="" />
-      </section>
-    </main>
+          <section className="infoImage">
+            <img src="" alt="" />
+          </section>
+        </main>
+      )}
+    </Slot>
   )
 }
