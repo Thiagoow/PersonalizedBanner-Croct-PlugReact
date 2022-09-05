b7af75177c9d178faa87529d5a1a0673f9cae3a0
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-05 17:27:32 -0300

    Btn styles set as reusable class (with custom p)

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,7 @@ import React from 'react'
 
 const Button = ({ label, href }) => {
   return (
-    <a
-      href={href}
-      target="_blank"
-      className={
-        'max-w-fit p-4 rounded-sm bg-firstColor text-btnTxtColor font-size font-semibold hover:bg-altFirstColor'
-      }
-    >
+    <a href={href} target="_blank" className={'primaryBtn p-4'}>
       {label}
     </a>
   )
