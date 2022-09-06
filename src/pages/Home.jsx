ba9da84c391c434f5c18dc01dd5337756bf9d3ed
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-06 14:12:50 -0300

    Added bannerImg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { Slot } from '@croct/plug-react'
 import Loading from '../components/Loading'
 import Button from '../components/Button'
+//Images import:
+import developer from '../assets/img/Developer.png'
+import marketer from '../assets/img/Marketer.png'
+import growthHacker from '../assets/img/GrowthHacker.png'
 
 export default function Home() {
   const fallbackBanner = {
@@ -17,19 +21,31 @@ export default function Home() {
     <React.Suspense fallback={<Loading />}>
       <Slot id="home-banner" fallback={fallbackBanner}>
         {({ title, subtitle, cta }) => (
-          <main className={'centerHVFlex bg-bodyColor text-txtColor'}>
-            <section className={'grid gap-4 max-w-sm'}>
+          <main
+            className={
+              'centerHVFlex bg-bodyColor text-txtColor mt-48 md:flex-row md:mt-0'
+            }
+          >
+            <section
+              className={'grid gap-4 max-w-sm ml-5 md:max-w-md md:ml-10'}
+            >
               <h1 className={'text-firstColor font-bold text-3xl'}>
-                <strong className="text-altFirstColor"></strong>
-                {title}
+                <strong className="text-altFirstColor">
+                  {title.split(' ').slice(0, 2).join(' ')}
+                </strong>{' '}
+                {title.split(' ').slice(2).join(' ')}
               </h1>
 
               <p>{subtitle}</p>
               <Button label={cta.label} href={cta.link} />
             </section>
 
-            <section className="infoImage">
-              <img src="" alt="" />
+            <section className={'mt-16 self-end md:mt-0 md:ml-16'}>
+              <img
+                src={developer}
+                className={'max-w-[17rem] xs:max-w-sm md:max-w-md xl:!max-w-lg'}
+                alt="persona image"
+              />
             </section>
           </main>
         )}
