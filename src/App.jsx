8324f759606fe958b8e77bb4e79504ab42f6b61c
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-06 20:02:02 -0300

    This isn't what it's causing req credentials missing error

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,10 @@ import { CroctProvider } from '@croct/plug-react'
 import { AppRoutes } from './Routes'
 
 const App = () => {
+  const noAccountId = '00000000-0000-0000-0000-000000000000'
+
   return (
-    <CroctProvider appId="00000000-0000-0000-0000-000000000000">
+    <CroctProvider appId={noAccountId}>
       <AppRoutes />
     </CroctProvider>
   )
