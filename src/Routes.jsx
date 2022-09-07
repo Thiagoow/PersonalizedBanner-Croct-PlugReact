c1e8ae37a086a5d32555f6d8ef477d42a3aff81b
Thiago Silva Lopes <thiagoslopes8@outlook.com>
2022-09-07 15:43:36 -0300

    Login page set as index

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
-import SetPersona from './pages/SetPersona'
+import Login from './pages/Login'
 
 export function AppRoutes() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} exact />
-          <Route path="/setPersona" element={<SetPersona />} exact />
+          <Route path="/" element={<Login />} exact />
+          <Route path="/home" element={<Home />} exact />
         </Routes>
       </Router>
     </>
